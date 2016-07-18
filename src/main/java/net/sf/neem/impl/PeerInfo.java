bed3d4894ddae1164e5dd7dc7f29dad91c4f5793
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-07-18 18:19:23 +0200

    Add constructor

--- src/main/java/net/sf/neem/impl/PeerInfo.java
+++ src/main/java/net/sf/neem/impl/PeerInfo.java
@@ -1,7 +1,26 @@
 package net.sf.neem.impl;
 
+import java.io.Serializable;
+import java.net.InetSocketAddress;
+import java.util.UUID;
+
 /**
  * Created by jocelyn on 18.07.16.
  */
-public class PeerInfo {
+class PeerInfo implements Serializable {
+    InetSocketAddress listen;
+    UUID id;
+    int age;
+
+    public PeerInfo(InetSocketAddress listen, UUID id, int age) {
+        this.listen = listen;
+        this.id = id;
+        this.age = age;
+    }
+
+    public PeerInfo(Connection conn) {
+        this.listen = conn.listen;
+        this.id = conn.id;
+        this.age = conn.age;
+    }
 }
